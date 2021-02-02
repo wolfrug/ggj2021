4cf9e6c3aa523ff9227b53f554cc6b7372b57645
petter_skult <petter.skult@gmail.com>
2021-02-02 14:51:52 +0200

    bugged not-working webgl version

---
+++ GGJ2021/Assets/Scripts/FMOD_Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMOD_Initializer : MonoBehaviour {
+    // Start is called before the first frame update
+    IEnumerator Start () {
+        yield return new WaitForSeconds (1f);
+        yield return new WaitUntil (() => FMODUnity.RuntimeManager.HasBankLoaded ("Master"));
+        Debug.Log ("Master Bank Loaded");
+        UnityEngine.SceneManagement.SceneManager.LoadScene ("MainMenu", UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+}
